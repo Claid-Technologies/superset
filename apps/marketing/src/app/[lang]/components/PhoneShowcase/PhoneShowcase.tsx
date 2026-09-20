0ef8be4aad640caadb4bc28e44a1f62a92435d90
Kiet Ho <hoakiet98@gmail.com>
2026-09-20 13:42:37 -0700

    fix(marketing): isolate and scale the mobile phone demo

--- apps/marketing/src/app/[lang]/components/PhoneShowcase/PhoneShowcase.tsx
+++ apps/marketing/src/app/[lang]/components/PhoneShowcase/PhoneShowcase.tsx
@@ -1,39 +1,10 @@
-import { PhoneFrame } from "./components/PhoneFrame";
-import { ReviewScreen } from "./components/ReviewScreen";
-import { SessionScreen } from "./components/SessionScreen";
 import { SpreadPhones } from "./components/SpreadPhones";
-import { WorkspacesScreen } from "./components/WorkspacesScreen";
-
-const SIDE_PHONE_CLASS =
-	"absolute top-10 hidden scale-[0.88] opacity-60 sm:block lg:hidden xl:block";
+import { StackedPhones } from "./components/StackedPhones";
 
 interface PhoneShowcaseProps {
 	layout?: "stack" | "spread";
 }
 
 export function PhoneShowcase({ layout = "stack" }: PhoneShowcaseProps) {
-	if (layout === "spread") {
-		return <SpreadPhones />;
-	}
-
-	return (
-		<div
-			aria-hidden="true"
-			className="pointer-events-none relative flex select-none justify-center"
-		>
-			<PhoneFrame
-				className={`${SIDE_PHONE_CLASS} right-1/2 origin-right translate-x-[-72px] -rotate-6`}
-			>
-				<SessionScreen />
-			</PhoneFrame>
-			<PhoneFrame
-				className={`${SIDE_PHONE_CLASS} left-1/2 origin-left translate-x-[72px] rotate-6`}
-			>
-				<ReviewScreen />
-			</PhoneFrame>
-			<PhoneFrame className="relative">
-				<WorkspacesScreen />
-			</PhoneFrame>
-		</div>
-	);
+	return layout === "spread" ? <SpreadPhones /> : <StackedPhones />;
 }
